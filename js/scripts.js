af0ccf671a60d0684522fa1c4fbe012e8f2d515e
Mifene95 <mifene95@gmail.com>
2026-04-26 13:11:44 +0200

    mostrar datos

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,43 @@ $(document).ready(function(){
             },
             success: function(respuesta){
                 console.log(respuesta);
+                cargarTareas();
             },
             error: function(){
                 console.log("Error en la peticion")
             }
         })
-            
-        
     });
+
+    cargarTareas();
+
+    function cargarTareas(){
+
+        $.ajax({
+            url: 'api/listar.php',
+            method: 'GET',
+
+            success: function(tareas){
+                $('#tabla-tareas').empty();
+                $.each(tareas, function(indice, tarea){
+                    let fila = `
+                    <tr>
+                    <td>${tarea.id}</td>
+                    <td>${tarea.nombre}</td>
+                    <td>${tarea.estado}</td>
+                    <td>
+                    <button class="btn btn-sm btn-danger">Borrar</button>
+                    <button class="btn btn-sm btn-success">Completar</button>
+                </td>
+            </tr>
+                    `
+                $('#tabla-tareas').append(fila);
+
+                }
+        )}
+            
+        })
+    }
 
 });
 
